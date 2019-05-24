df00211576da9f1caf335af7bac114f54450a3cd
nirvik saha <nirviksaha@lawn-143-215-85-221.lawn.gatech.edu>
2019-05-24 17:47:01 -0400

    check cync

--- UFG/UFGInputs.cs
+++ UFG/UFGInputs.cs
@@ -23,32 +23,17 @@ namespace UFG
             // 1.
             pManager.AddTextParameter("Input-Setbacks", "corresponding-street-setbacks", "Corresponding Input for setbacks: Enter numbers separated by comms\nPreferred Input Type: write text in PANEL", GH_ParamAccess.item);
             // 2.
-            pManager.AddCurveParameter("Input-Site-Curves", "inp.B.0", "Select all site boundaries: closed planar curves (polylines, nurbs, etc)\nPreferred Input Type: CRV & select multiple curves", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Input-Site-Curves", "inp-site-crvs", "Select all site boundaries: closed planar curves (polylines, nurbs, etc)\nPreferred Input Type: CRV & select multiple curves", GH_ParamAccess.list);
             // 3. 
-            pManager.AddNumberParameter("Input-FSR", "inp.C.0", "Default Value=2.5\nEnter a number for FSR calculations\nPreferred Input Type: NUMERIC SLIDER", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Input-FSR", "input-fsr", "Default Value=2.5\nEnter a number for FSR calculations\nPreferred Input Type: NUMERIC SLIDER", GH_ParamAccess.item);
             }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             // 0.
-            // pManager.AddTextParameter("Output-Street-Names", "out.A.0.0", "street layer names", GH_ParamAccess.list);
+            pManager.AddGeometryParameter("Output-Solid-Geometry", "out-solid-Geom", "outputs solid geometry -breps, surfaces, mesh convertible", GH_ParamAccess.list);
             // 1.
-            // pManager.AddTextParameter("Output-Setback-Numbers", "out.A.0.1", "setback numbers", GH_ParamAccess.list);
-            // 2.
-            // pManager.AddCurveParameter("Output-Street-Curve", "out.A.2", "List of street curves from layer names for streets", GH_ParamAccess.list);
-            // 3.
-            // pManager.AddTextParameter("Output-Proc-Object", "out.A.3", "Process object ready for geometric processing", GH_ParamAccess.list);
-            // 4.
-            // pManager.AddLineParameter("Intx-Rays", "Intx-Rays", "Intx-Rays", GH_ParamAccess.list);
-            // 5.
-            // pManager.AddPointParameter("intx", "intx", "intx", GH_ParamAccess.list);
-            // 6.
-            // pManager.AddCurveParameter("site", "site", "site", GH_ParamAccess.list);
-            // 7.
-            pManager.AddGeometryParameter("Output-Solid-Geometry", "out.A.0.0", "outputs solid geometry -breps, surfaces, mesh convertible", GH_ParamAccess.list);
-            // 1.
-            pManager.AddTextParameter("Output-SITE-Numbers", "out.A.0.1", "valid site numbers", GH_ParamAccess.item);
-             
+            pManager.AddTextParameter("Output-SITE-Numbers", "debug-numbers", "valid site numbers", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
@@ -61,29 +46,16 @@ namespace UFG
 
             InputProc inputproc = new InputProc(streetLayerNames, setbacks);
 
-            // List<string> STREET_LAYER_NAMES = inputproc.GetStreetNames();
-            // List<double> SETBACKS = inputproc.GetSetbacks();
-            /// DA.SetDataList(0, STREET_LAYER_NAMES); // SET THE STRING : LAYER NAMES
-            /// DA.SetDataList(1, SETBACKS); // SET THE DOUBLE - SETBACKS
-
             inputproc.GetLayerGeom();
-            // List<LineCurve> STREETLINES = inputproc.GetLinesFromLayers();
-            /// DA.SetDataList(2, STREETLINES); // SET THE STREET LINE : CURVES
-
             List<ProcObj> PROCOBJLI = inputproc.GetProcObjs();
-            // List<string> PROCOBJLIStr = inputproc.GetProcObjLiString();
-            /// DA.SetDataList(3, PROCOBJLIStr); // SET THE PROC-OBJ-LI-STRING : STRING 
-
             List<Curve> tempSITES = new List<Curve>();
             double FSR = 2.5;
             double MAXHT = 50.0;
             double MINHT = 0.0;
             DA.GetDataList(2, tempSITES);
             DA.GetData(3, ref FSR);
-            // DA.GetData(4, ref MAXHT);
-            // DA.GetData(5, ref MINHT);
-
             List<Curve> SITES = new List<Curve>();
+
             // check and eliminate bad curves
             for (int i=0; i<tempSITES.Count; i++)
             {
